ae9a50d00003e5aed0aa198e9171c73b2cb19823
yamamotoayami <a_chu57@yahoo.co.jp>
2018-05-31 13:32:31 +0900

    Update cond.c

--- 01/cond.c
+++ 01/cond.c
@@ -3,12 +3,18 @@
 
 int main(void)
 {
-    int array[20];
-    for (int i = 0; i < 20; ++i)
+    int a[20];
+    int i;
+    for ( i = 0; i < 20; i++)
     {
-        array[i] = random();
+        a[i] = rand();
+   
+    
+    if (a[i] % 2 == 0) 
+    {
+    printf("%2d ,",a[i] );
     }
-
+}
     // TODO: 上記のとおりランダムに生成した20個の
     // 整数を格納した配列から，偶数のみを選択して
     // コンソールに出力するプログラムを作成．
